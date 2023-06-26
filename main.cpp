88c24541926e69176dcc0a06229fe3de605464f9
pichim <michernstpet@gmail.com>
2023-06-26 17:43:11 +0200

    Added bf bbl log

--- main.cpp
+++ main.cpp
@@ -7,28 +7,26 @@
 
 int main(int argc, char *argv[])
 {
-    float data[17];
-    FILE *dataInputFilePtr = fopen("input/putty_16.log", "r");  
+    float data[6]; // mag, gyro
+    FILE *dataInputFilePtr = fopen("input/data_20230626_bf_LOG087.txt", "r");  
 
     std::ofstream dataOutputFile("output/mag_calib.txt");
 
     magBiasEstimatorRLS_t magBiasEstimatorRLS;
-    magBiasEstimatorRLSInit(&magBiasEstimatorRLS, 0.96f, 2.0e1f);
+    magBiasEstimatorRLSInit(&magBiasEstimatorRLS, 0.99f, 1.0e0f);
     magBiasEstimatorNLO_t magBiasEstimatorNLO;
     magBiasEstimatorNLOInit(&magBiasEstimatorNLO, 20.0f, 0.2f, 20000);
     static float mag[3], dmag[3], gyro[3];
     static float magPast[3] = {0.0f, 0.0f, 0.0f};
-    static float Ts = 0.02f;
+    static float Ts = 0.1f;
 
     static bool is_first = true;
 
-    while( fscanf(dataInputFilePtr, "%f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f",
-                                    &data[ 0], &data[ 1], &data[ 2], &data[ 3], &data[ 4], &data[ 5], &data[ 6],
-                                    &data[ 7], &data[ 8], &data[ 9], &data[10], &data[11], &data[12], &data[13],
-                                    &data[14], &data[15], &data[16]) != EOF ) {
+    while( fscanf(dataInputFilePtr, "%f, %f, %f, %f, %f, %f",
+                                    &data[0], &data[1], &data[2], &data[3], &data[4], &data[5]) != EOF ) {
         
-        mag[0]  = data[6];  mag[1] = data[7];  mag[2] = data[8];
-        gyro[0] = data[0]; gyro[1] = data[1]; gyro[2] = data[2];
+        mag[0]  = data[0];  mag[1] = data[1];  mag[2] = data[2];
+        gyro[0] = data[3]; gyro[1] = data[4]; gyro[2] = data[5];
 
         if (is_first) {
             for (uint8_t i = 0; i < 3; i++) {
