3c2903e6a9f2a777e1c9dbcad43da21d06953614
pichim <michernstpet@gmail.com>
2023-10-09 16:51:26 +0200

    Matches SHA-1: 201efe8

--- include/magnetometer_calibration.h
+++ include/magnetometer_calibration.h
@@ -1,32 +1,13 @@
-/*
- * This file is part of Cleanflight and Betaflight.
- *
- * Cleanflight and Betaflight are free software. You can redistribute
- * this software and/or modify this software under the terms of the
- * GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * Cleanflight and Betaflight are distributed in the hope that they
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this software.
- *
- * If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
 typedef struct compassBiasEstimator_s {
-    float lambda_min, lambda, p0;
+    float lambda_min, lambda;
     float b[3];
-    float P[3][3];
+    float theta[4];
+    float U[4][4];
+    float D[4];
 } compassBiasEstimator_t;
 
 void compassBiasEstimatorInit(compassBiasEstimator_t *compassBiasEstimator, const float lambda_min, const float p0);
 void compassBiasEstimatorUpdate(compassBiasEstimator_t *compassBiasEstimator, const float lambda_min, const float p0);
-void compassBiasEstimatorApply(compassBiasEstimator_t *compassBiasEstimator, float *mag, const float *dmag, const float *gyro);
-void compassBiasEstimatorSolveIterative(compassBiasEstimator_t *compassBiasEstimator, float *zn, const float *e, const float *gyro, const unsigned k, const unsigned i, const unsigned j, const float sign);
+void compassBiasEstimatorApply(compassBiasEstimator_t *cBE, float *mag, const float scaleMag);
--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 #include "axis.h"
 #include "magnetometer_calibration.h"
 
-#define LAMBDA_MIN 0.995f                                    // minimal adaptive forgetting factor, range: [0, 1]
-#define P0 1.0e1f                                            // value to initialize P(0) = diag([P0, P0, P0]), typically in range: (0, 1000)
+#define LAMBDA_MIN 0.99f                                     // minimal adaptive forgetting factor, range: [0, 1]
+#define P0 1.0e0f                                            // value to initialize P(0) = diag([P0, P0, P0]), typically in range: (0, 1000)
+#define SCALE_MAG 5.0e2f                                     // scale for magnetometer data, method is sensitive, this was optimized for raw mag data
+                                                             // in the range (1000, 2000)
 
-#define TASK_COMPASS_RATE_HZ 10
-
-// run in terminal: .\output\mag_calib.exe "input/20230911_apex5_mag_on_head_00.txt"
+// run in terminal: .\output\mag_calib.exe "input/20231008_apex5_mag_on_tpu_00.txt"
 
 int main(int argc, char *argv[])
 {
@@ -18,9 +18,7 @@ int main(int argc, char *argv[])
         float magADC[XYZ_AXIS_COUNT];
     } mag_t;
 
-    static mag_t mag, magPrevious;
-
-    const float actualCompassRateHz = TASK_COMPASS_RATE_HZ;
+    static mag_t mag;
 
     float data[6]; // mag, gyro
     FILE *dataInputFilePtr = fopen(argv[1], "r");
@@ -35,36 +33,16 @@ int main(int argc, char *argv[])
         
         for (int axis = 0; axis < XYZ_AXIS_COUNT; axis++) {
             mag.magADC[axis] = data[axis];
-        }
-
-        // avoid initial transient phase
-        static bool isFirstMagMeasurement = true;
-        if (isFirstMagMeasurement) {
-            isFirstMagMeasurement = false;
-            for (int axis = 0; axis < XYZ_AXIS_COUNT; axis++) {
-                magPrevious.magADC[axis] = mag.magADC[axis];
-            }
-        }
-
-        // calculate mag derivative
-        const float magDerivative[XYZ_AXIS_COUNT] = {(mag.magADC[X] - magPrevious.magADC[X]) * actualCompassRateHz,
-                                                     (mag.magADC[Y] - magPrevious.magADC[Y]) * actualCompassRateHz,
-                                                     (mag.magADC[Z] - magPrevious.magADC[Z]) * actualCompassRateHz};
-
-        // get downsampled gyro data
-        const float gyroAverageRadians[XYZ_AXIS_COUNT] =  {data[3], data[4], data[5]};        
-
-        // copy mag to be able to build mag derivative
-        magPrevious = mag;
+        }   
 
         // run compassBiasEstimatorApply and measure time
         std::chrono::steady_clock::time_point time_begin_ns = std::chrono::steady_clock::now();        
-        compassBiasEstimatorApply(&compassBiasEstimator, mag.magADC, magDerivative, gyroAverageRadians);
+        compassBiasEstimatorApply(&compassBiasEstimator, mag.magADC, SCALE_MAG);
         int64_t time_elapsed_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(time_begin_ns - std::chrono::steady_clock::now()).count();
 
         // print results to terminal and file
-        std::cout      << compassBiasEstimator.b[0] << ", " << compassBiasEstimator.b[1] << ", "
-                       << compassBiasEstimator.b[2] << ", " << compassBiasEstimator.lambda << ", " << time_elapsed_ns << std::endl;
+        //std::cout      << compassBiasEstimator.b[0] << ", " << compassBiasEstimator.b[1] << ", "
+        //               << compassBiasEstimator.b[2] << ", " << compassBiasEstimator.lambda << ", " << time_elapsed_ns << std::endl;
 
         // print results to file
         dataOutputFile << compassBiasEstimator.b[0] << ", " << compassBiasEstimator.b[1] << ", "
